d182b298e81d067378c705ce0f00462ed11ead44
김승진 <kimsj1993@gmail.com>
2018-12-01 19:29:25 -0600

    add simple Ui for login page

--- stack/app/front-end/views/LoginForm.js
+++ stack/app/front-end/views/LoginForm.js
@@ -1,16 +1,75 @@
 import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
 
-const LoginForm = ({ value, handleChange, handleLogin }) => (
-	<form onSubmit={ handleLogin } >
-		<TextField 
-			variant='outlined'
-			placeholder='Choose a name' 
-			value={ value } 
-			onChange={ handleChange }
-		/>
-		<Button type='submit'>Start</Button>
-	</form>
+import Button from '@material-ui/core/Button';
+import {withStyles} from "@material-ui/core";
+
+const styles = theme => ({
+	root: {
+
+        marginTop: 48,
+        maxWidth: 600,
+		backgroundColor:'#9c27b0'
+
+    },
+	container: {
+        display: 'grid',
+        gridTemplateColumns: '1fr 1fr 1fr',
+        gridGap: '10px',
+        padding: '10px',
+	},
+
+	item1 : {
+		gridColumnStart: '1',
+		gridColumnEnd: '4',
+        backgroundColor: "rgba(255,255,255, 0.8)",
+        textAlign: 'center',
+        padding: '20px 0',
+        fontSize: '30px'
+	},
+
+	item2 : {
+		gridColumnStart: '1',
+		gridColumnEnd: '2',
+        backgroundColor: "rgba(255,255,255, 0.8)",
+        textAlign: 'center',
+        padding: '20px 0',
+        fontSize: '30px'
+	},
+    item3 : {
+        gridColumnStart: '2',
+        gridColumnEnd: '4',
+        backgroundColor: "rgba(255,255,255, 0.8)",
+        textAlign: 'center',
+        padding: '20px 0',
+        fontSize: '30px'
+    }
+
+
+});
+
+const LoginForm = ({classes, value, handleChange, handleLogin }) => (
+	<div className={classes.root}>
+		<div className={classes.container}>
+			<div className={classes.item1}>
+				Welcome to Fluxx
+			</div>
+			<div className={classes.item2}>
+				Username
+			</div>
+			<div className={classes.item3}>
+				<form onSubmit={ handleLogin } >
+					<TextField
+						variant='outlined'
+						placeholder='Username'
+						floatingLabelText = "Username"
+						value={ value }
+						onChange={ handleChange }
+					/>
+					<Button color='primary' type='submit'>Start</Button>
+				</form>
+			</div>
+		</div>
+	</div>
 );
 
-export default LoginForm;
+export default withStyles(styles)(LoginForm);
