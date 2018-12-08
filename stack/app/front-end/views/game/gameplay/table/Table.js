d6cae8ec114f8de6d8b80f5a962793d7b7104ac8
김승진 <kimsj1993@gmail.com>
2018-12-08 17:03:46 -0600

    Get rid of keeper card in table component and replace that by end turn button

--- stack/app/front-end/views/game/gameplay/table/Table.js
+++ stack/app/front-end/views/game/gameplay/table/Table.js
@@ -3,7 +3,11 @@ import { withStyles } from '@material-ui/core/styles';
 import DeckContainer from './DeckContainer';
 import HandContainer from './HandContainer';
 import DiscardContainer from './DiscardContainer';
-import PlayerKeepers from '../KeepersContainer';
+import Fab from '@material-ui/core/Fab';
+import Button from "@material-ui/core/Button/Button";
+import Paper from "@material-ui/core/Paper/Paper";
+
+
 
 const styles = theme => ({
 	root: {
@@ -21,16 +25,24 @@ const styles = theme => ({
 	hand: {
 		flexGrow: 2
 	},
-	keepers: {
-		width: 416
+	buttonsContainer: {
+		display:'grid',
+        gridTemplateColumns: 'auto',
+		width: 416,
 	},
+	buttonItem : {
+        border: '1px solid rgba(0, 0, 0, 0.8)',
+		padding: '20px',
+		margin: '20px',
+        textAlign: 'center'
+},
 	center: {
 		display: 'flex',
 		justifyContent: 'center'
 	}
 });
 
-const Table = ({ classes, keeperIds }) => (
+const Table = ({ classes, isTurn, canEndTurn, endTurn }) => (
 	<div className={ classes.root } >
 		<div className={ classes.discard } >
 			<DiscardContainer />
@@ -43,10 +55,8 @@ const Table = ({ classes, keeperIds }) => (
 				<HandContainer />
 			</div>
 		</div>
-		<div className={ classes.keepers } >
-			<div className={ classes.center }>
-				<PlayerKeepers keeperIds={ keeperIds } />
-			</div>
+		<div className={classes.buttonsContainer}>
+            { (isTurn) ? (<Fab variant='extended' color='primary'  className={classes.buttonItem} disabled={ !canEndTurn } onClick={ endTurn } >End Turn</Fab>) : ''}
 		</div>
 	</div>
 );
--- stack/app/front-end/views/game/gameplay/table/TableContainer.js
+++ stack/app/front-end/views/game/gameplay/table/TableContainer.js
@@ -2,20 +2,34 @@ import { connect } from 'react-redux';
 
 import { userSelectors } from '../../../../state/modules/user';
 import { playersSelectors } from '../../../../state/modules/data/players';
+import { gameSelectors, gameOperations } from '../../../../state/modules/data/game';
+
 
 import Table from './Table';
+import * as usersSelectors from "../../../../state/modules/data/users/selectors";
 
 const mapStateToProps = (state, ownProps) => {
-	const userId = userSelectors.getUserId( state );
-	const keeperIds = playersSelectors.getPlayerKeeperIds( state, userId );
-	
-	return { keeperIds };
+    const userId = userSelectors.getUserId( state );
+    const isTurn = gameSelectors.isPlayerTurn( state, userId );
+    const canEndTurn = true;
+
+    return {
+        isTurn,
+        canEndTurn
+    };
 };
+
+const mapDispatchToProps = dispatch => ( {
+    endTurn: () => dispatch( gameOperations.endTurn() )
+} );
+
 
 const TableContainer = (props) => (
 	<Table {...props} />
 );
 
 export default connect(
-	mapStateToProps
+	mapStateToProps,
+    mapDispatchToProps
+
 )(TableContainer);
